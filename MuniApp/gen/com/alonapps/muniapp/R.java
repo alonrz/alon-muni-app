aedc21f5ba10b93e02ca9844fc708db814825ea6
alon <alon.zilberman@gmail.com>
2013-03-16 21:56:34 -0700

    finished showing a list of all routes after clicking  a button on the main activity. This tests the XML fetching via thread

--- MuniApp/gen/com/alonapps/muniapp/R.java
+++ MuniApp/gen/com/alonapps/muniapp/R.java
@@ -28,6 +28,7 @@ public final class R {
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int single_list_item=0x7f030001;
     }
     public static final class menu {
         public static final int main=0x7f070000;
@@ -36,6 +37,11 @@ public final class R {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
+        public static final int show_lines=0x7f050003;
+        public static final int xml_agency_name_tag=0x7f050007;
+        public static final int xml_agency_sfmuni=0x7f050005;
+        public static final int xml_base_command=0x7f050004;
+        public static final int xml_route_list_command=0x7f050006;
     }
     public static final class style {
         /** 
--- MuniApp/src/com/alonapps/muniapp/MainActivity.java
+++ MuniApp/src/com/alonapps/muniapp/MainActivity.java
@@ -1,15 +1,36 @@
 package com.alonapps.muniapp;
 
+import java.util.List;
+
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
+//	/**inner class UIHandler **/
+//	private final class UIHandler extends Handler {
+//		 
+//        public void handleMessage(Message msg) {
+//            
+//            
+//        }
+// 
+//    };
+    
+    
+    
+	XmlFetcher fetcher; 
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
     }
 
 
@@ -18,6 +39,26 @@ public class MainActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+    
+    
+//    final private Handler handler = new UIHandler();
+    
+    public void onClick(View v)
+    {
+//    	new Thread(new Runnable() {
+//			
+//			@Override
+//			public void run() {
+//				// TODO Auto-generated method stub
+//				List<String> routeList = fetcher.GetRouteList();		
+//			}
+//		});
+    	
+    	Intent intent = new Intent(this, ListV.class);
+//    	intent.putExtra("routeList", routeList.toArray(new String[0]));
+    	startActivity(intent);
+    	
     }
     
 }
