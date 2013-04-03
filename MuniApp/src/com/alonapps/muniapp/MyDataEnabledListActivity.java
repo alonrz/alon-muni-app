5a96ac752361db19af691785088018790eca4213
alon <alon.zilberman@gmail.com>
2013-04-02 19:23:18 -0700

    Removed context field. each class will take care of its own

--- MuniApp/src/com/alonapps/muniapp/MyDataEnabledListActivity.java
+++ MuniApp/src/com/alonapps/muniapp/MyDataEnabledListActivity.java
@@ -6,17 +6,15 @@ import android.os.Bundle;
 
 public class MyDataEnabledListActivity extends ListActivity {
 
-	Context context;
 	DataManager mDataManager = null;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		
-		context = this;
 		if(mDataManager == null)
 		{
-			mDataManager= DataManager.getDataManager(context);
+			mDataManager= DataManager.getDataManager(this);
 		}
 		
 	}
